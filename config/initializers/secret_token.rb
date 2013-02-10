be3c2ca686b60c4b479c70161ff36c6d72ee7544
Prithvi Sharma <prithsharma100@gmail.com>
2013-02-10 12:25:59 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrithviApp1::Application.config.secret_token = '531df8db3a9de67e36daad970a3168752d6c0858b25d999385aa5adeb3ce1a84f5e6b15028720b915aa26f90694cb34e131e4212ac412376d2efed6599b925f8'
